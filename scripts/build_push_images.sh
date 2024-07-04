df3211314eb5ff0c4e311634dcda29ebffb65e05
ZihaoWang <zihaowangbupt@gmail.com>
2024-07-05 01:02:21 +0200

    finish code

--- scripts/build_push_images.sh
+++ scripts/build_push_images.sh
@@ -4,8 +4,8 @@ sudo docker login docker.io
 
 DOCKER_HUB_USERNAME=evensong
 
-sudo docker build -t $DOCKER_HUB_USERNAME/flask-app:latest -f Dockerfile .
-sudo docker build -t $DOCKER_HUB_USERNAME/model-training:latest -f Dockerfile.train .
+sudo docker build -t $DOCKER_HUB_USERNAME/flask-app:latest -f src/app/Dockerfile .
+sudo docker build -t $DOCKER_HUB_USERNAME/model-training:latest -f src/training/Dockerfile.train .
 
 sudo docker push $DOCKER_HUB_USERNAME/flask-app:latest
 sudo docker push $DOCKER_HUB_USERNAME/model-training:latest
